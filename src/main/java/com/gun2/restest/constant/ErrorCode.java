a360900b1ac57b6ccf35cf04307ba9546372b242
gun2 <0dayz@naver.com>
2022-07-04 23:15:39 +0900

    추가(상태코드): Response 상태코드 관련 클래스 추가

---
+++ src/main/java/com/gun2/restest/constant/ErrorCode.java
@@ -0,0 +1,37 @@
+package com.gun2.restest.constant;
+
+public enum ErrorCode implements ResponseCode{
+
+    INVALID_INPUT_VALUE(400, "INVALID_INPUT_VALUE", " Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405,"METHOD_NOT_ALLOWED", " Invalid Input Value"),
+    ENTITY_NOT_FOUND(400,"ENTITY_NOT_FOUND", " Entity Not Found"),
+    INTERNAL_SERVER_ERROR(500,"INTERNAL_SERVER_ERROR", "Server Error"),
+    INVALID_TYPE_VALUE(400,"INVALID_TYPE_VALUE", " Invalid Type Value"),
+    HANDLE_ACCESS_DENIED(403 ,"HANDLE_ACCESS_DENIED", "Access is Denied");
+
+    private int status;
+    private String code;
+    private String message;
+
+    ErrorCode(int status, String code, String message){
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+
+    @Override
+    public int getStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
