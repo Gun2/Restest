0d29a43533b8b7615d12d6f39d45bef595807a2d
gun2 <0dayz@naver.com>
2022-07-06 02:16:17 +0900

    추가(상태코드): 400(ID_IS_NULL) 상태코드 추가. Id 검색에서 Id가 null인 경우 사용.

--- src/main/java/com/gun2/restest/constant/ErrorCode.java
+++ src/main/java/com/gun2/restest/constant/ErrorCode.java
@@ -2,10 +2,11 @@ package com.gun2.restest.constant;
 
 public enum ErrorCode implements ResponseCode{
 
-    INVALID_INPUT_VALUE(400, "INVALID_INPUT_VALUE", " Invalid Input Value"),
-    METHOD_NOT_ALLOWED(405,"METHOD_NOT_ALLOWED", " Invalid Input Value"),
-    ENTITY_NOT_FOUND(400,"ENTITY_NOT_FOUND", " Entity Not Found"),
-    NOT_FOUNT_BY_ID(400,"NOT_FOUNT_BY_ID", " Not Found By Id"),
+    INVALID_INPUT_VALUE(400, "INVALID_INPUT_VALUE", "Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405,"METHOD_NOT_ALLOWED", "Invalid Input Value"),
+    ENTITY_NOT_FOUND(400,"ENTITY_NOT_FOUND", "Entity Not Found"),
+    NOT_FOUND_BY_ID(400,"NOT_FOUND_BY_ID", "Not Found By Id"),
+    ID_IS_NULL(400,"ID_IS_NULL", "Not Receiving Identify Value"),
     INTERNAL_SERVER_ERROR(500,"INTERNAL_SERVER_ERROR", "Server Error"),
     INVALID_TYPE_VALUE(400,"INVALID_TYPE_VALUE", " Invalid Type Value"),
     HANDLE_ACCESS_DENIED(403 ,"HANDLE_ACCESS_DENIED", "Access is Denied");
