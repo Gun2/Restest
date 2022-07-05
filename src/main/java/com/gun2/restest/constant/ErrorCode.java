168c2949d3d33ab00cb0e2fbae3f4a948b052b0b
gun2 <0dayz@naver.com>
2022-07-06 01:31:35 +0900

    추가(상태코드): 상태코드 추가. NOT_FOUNT_BY_ID(DB에 Id질의 결과 찾는 값이 없으면 사용).

--- src/main/java/com/gun2/restest/constant/ErrorCode.java
+++ src/main/java/com/gun2/restest/constant/ErrorCode.java
@@ -5,6 +5,7 @@ public enum ErrorCode implements ResponseCode{
     INVALID_INPUT_VALUE(400, "INVALID_INPUT_VALUE", " Invalid Input Value"),
     METHOD_NOT_ALLOWED(405,"METHOD_NOT_ALLOWED", " Invalid Input Value"),
     ENTITY_NOT_FOUND(400,"ENTITY_NOT_FOUND", " Entity Not Found"),
+    NOT_FOUNT_BY_ID(400,"NOT_FOUNT_BY_ID", " Not Found By Id"),
     INTERNAL_SERVER_ERROR(500,"INTERNAL_SERVER_ERROR", "Server Error"),
     INVALID_TYPE_VALUE(400,"INVALID_TYPE_VALUE", " Invalid Type Value"),
     HANDLE_ACCESS_DENIED(403 ,"HANDLE_ACCESS_DENIED", "Access is Denied");
