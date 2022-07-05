281fb35f2ef788e9b08762ca05a8bfb46c50545f
gun2 <0dayz@naver.com>
2022-07-06 02:17:52 +0900

    추가(업무관리): 업무관리 Controller 추가.

---
+++ src/main/java/com/gun2/restest/controller/rest/JobRestController.java
@@ -0,0 +1,34 @@
+package com.gun2.restest.controller.rest;
+
+import com.gun2.restest.constant.SuccessCode;
+import com.gun2.restest.dto.JobDto;
+import com.gun2.restest.form.response.SuccessResponse;
+import com.gun2.restest.service.JobService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class JobRestController {
+    private final JobService jobService;
+
+    @GetMapping(path = "/v1/job/{id}")
+    public ResponseEntity findById(@PathVariable Long id){
+        JobDto jobDto = jobService.findById(id);
+        return new SuccessResponse(jobDto).toResponseEntity(SuccessCode.OK);
+    }
+
+    @PostMapping(path = "/v1/job")
+    public ResponseEntity create(@RequestBody @Validated JobDto jobDto){
+        JobDto result = jobService.insert(jobDto);
+        return new SuccessResponse(result).toResponseEntity(SuccessCode.CREATED);
+    }
+}
