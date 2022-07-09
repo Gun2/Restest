e947c206e7d8a2537326a2bba39f6704e411c6d4
gun2 <0dayz@naver.com>
2022-07-09 21:23:52 +0900

    수정(예외처리): Exception handler 주석 설명 추가.

--- src/main/java/com/gun2/restest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gun2/restest/exception/GlobalExceptionHandler.java
@@ -21,18 +21,35 @@ import java.nio.file.AccessDeniedException;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    /**
+     * <b>{@link org.springframework.web.bind.annotation.RequestBody}로 받은 값 중 Validation 실패 시 발생</b>
+     * @param e {@link MethodArgumentNotValidException}
+     * @return {@link ResponseEntity}
+     * @see org.springframework.validation.annotation.Validated
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error("handleMethodArgumentNotValidException", e);
         return ErrorResponse.of(e.getFieldErrors()).toResponseEntity(ErrorCode.INVALID_INPUT_VALUE);
     }
 
+    /**
+     * <b>{@link org.springframework.web.bind.annotation.ModelAttribute}로 받은 값 중 validation 실패 시 발생</b>
+     * @param e {@link BindException}
+     * @return {@link ResponseEntity}
+     * @see org.springframework.validation.annotation.Validated
+     */
     @ExceptionHandler(BindException.class)
     protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
         log.error("handleBindException", e);
         return ErrorResponse.of(e.getFieldErrors()).toResponseEntity(ErrorCode.INVALID_INPUT_VALUE);
     }
 
+    /**
+     * <b>지원하지 않는 메소드 호출 시 발생</b>
+     * @param e {@link HttpRequestMethodNotSupportedException}
+     * @return {@link ResponseEntity}
+     */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         log.error("handleHttpRequestMethodNotSupportedException", e);
@@ -40,9 +57,9 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 접근 권한이 없는 경우 발생
-     * @param e
-     * @return
+     * <b>접근 권한이 없는 경우 발생</b>
+     * @param e {@link AccessDeniedException}
+     * @return {@link ResponseEntity}
      */
     @ExceptionHandler(AccessDeniedException.class)
     protected ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
@@ -51,21 +68,21 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 없는 row를 질의하여 사용하려고 한 경우.
-     * @param e
-     * @return
+     * <b>DB table에 id로 데이터를 질의 하였으나 값을 찾을 수 없는경우 발생</b>
+     * @param e {@link RowNotFoundFromIdException}
+     * @return {@link ResponseEntity}
      */
-    @ExceptionHandler(RowNotFoundByIdException.class)
-    protected ResponseEntity<ErrorResponse> handleRowNotFoundByIdException(RowNotFoundByIdException e){
+    @ExceptionHandler(RowNotFoundFromIdException.class)
+    protected ResponseEntity<ErrorResponse> handleRowNotFoundByIdException(RowNotFoundFromIdException e){
         log.error("handleRowNotFoundByIdException", e);
         log.error("handleRowNotFoundByIdException id : {}", e.getId());
         return new ErrorResponse().toResponseEntity(ErrorCode.NOT_FOUND_BY_ID);
     }
 
     /**
-     * id가 null인 값으로 겁색을 요청한 경우.
-     * @param e
-     * @return
+     * <b>id가 null인 값으로 겁색을 요청한 경우.</b>
+     * @param e {@link IdentityIsNullException}
+     * @return {@link ResponseEntity}
      */
     @ExceptionHandler(IdentityIsNullException.class)
     protected ResponseEntity<ErrorResponse> handleIdentityIsNullException(IdentityIsNullException e){
@@ -74,9 +91,9 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 잘못된 타입으로 요청한 경우
-     * @param e
-     * @return
+     * <b>잘못된 타입으로 요청한 경우</b>
+     * @param e {@link MethodArgumentTypeMismatchException}
+     * @return {@link ResponseEntity}
      */
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
@@ -85,9 +102,9 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 지원하지 않는 Content type 으로 요청한 경우 발생
-     * @param e
-     * @return
+     * <b>지원하지 않는 Content type 으로 요청한 경우 발생</b>
+     * @param e {@link HttpMediaTypeNotSupportedException}
+     * @return {@link ResponseEntity}
      */
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     protected ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e){
@@ -96,9 +113,9 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 요청한 값을 읽을 수 없는 경우 발생 ex => JSON format이 옳바르지 않은경우
-     * @param e
-     * @return
+     * <b>요청한 값을 읽을 수 없는 경우 발생 ex => JSON format이 옳바르지 않은경우</b>
+     * @param e {@link HttpMessageNotReadableException}
+     * @return {@link ResponseEntity}
      */
     @ExceptionHandler(HttpMessageNotReadableException.class)
     protected ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
@@ -107,9 +124,9 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 요청한 값을 읽을 수 없는 경우 발생 ex => JSON format이 옳바르지 않은경우
-     * @param e
-     * @return
+     * <b>요청한 값을 읽을 수 없는 경우 발생 ex => JSON format이 옳바르지 않은경우</b>
+     * @param e {@link InvalidDataAccessApiUsageException}
+     * @return {@link ResponseEntity}
      */
     @ExceptionHandler(InvalidDataAccessApiUsageException.class)
     protected ResponseEntity<ErrorResponse> handleInvalidDataAccessApiUsageException(InvalidDataAccessApiUsageException e){
@@ -117,6 +134,11 @@ public class GlobalExceptionHandler {
         return new ErrorResponse().toResponseEntity(ErrorCode.INVALID_INPUT_VALUE);
     }
 
+    /**
+     * <b>기타 정의하지 않은 예외 발생 시</b>
+     * @param e {@link Exception}
+     * @return {@link ResponseEntity}
+     */
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e){
         return new ErrorResponse().toResponseEntity(ErrorCode.INTERNAL_SERVER_ERROR);
