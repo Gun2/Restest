9f9de83437833ca10afb25a155dd2d5890443a8b
gun2 <0dayz@naver.com>
2022-07-22 01:01:23 +0900

    추가(전역설정): 시스템 내부에서 사용할 전역 값 정의 클래스 추가.

---
+++ src/main/java/com/gun2/restest/constant/GlobalValue.java
@@ -0,0 +1,9 @@
+package com.gun2.restest.constant;
+
+/**
+ * <b>시스템 전역 변수</b>
+ */
+public class GlobalValue {
+    public static final int SCHEDULE_MIN_DELAY = 100;   //스케줄러 최소 딜레이
+    public static final int AFTER_BODY_MIN_DELAY = 100;  //body실행 이후 최소 딜레이
+}
