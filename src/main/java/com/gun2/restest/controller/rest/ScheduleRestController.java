92a51d87c0a8331ac6247ed3069f45da4ff6a179
gun2 <0dayz@naver.com>
2022-12-14 00:49:03 +0900

    수정(스케줄러): 스케줄러 DTO, Request form 모델 분리

--- src/main/java/com/gun2/restest/controller/rest/ScheduleRestController.java
+++ src/main/java/com/gun2/restest/controller/rest/ScheduleRestController.java
@@ -4,8 +4,11 @@ import com.gun2.restest.component.scheduler.SchedulerInfo;
 import com.gun2.restest.constant.SuccessCode;
 import com.gun2.restest.dto.ScheduleDto;
 import com.gun2.restest.dto.ScheduleRunDto;
+import com.gun2.restest.form.request.ScheduleRequest;
+import com.gun2.restest.form.request.ScheduleRunRequest;
 import com.gun2.restest.form.response.SuccessResponse;
 import com.gun2.restest.service.ScheduleService;
+import com.gun2.restest.spreader.ScheduleChangeDataSpreader;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,41 +27,45 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ScheduleRestController {
     private final ScheduleService scheduleService;
+    private final ScheduleChangeDataSpreader scheduleChangeDataSpreader;
 
     @ApiOperation(value = "모든 스케줄 조회", notes="스케줄 테이블의 모든 값을 반환")
     @GetMapping(path = "/v1/schedules")
-    public ResponseEntity findAll(){
+    public ResponseEntity<SuccessResponse> findAll(){
         List<ScheduleDto> items = scheduleService.findAll();
         return new SuccessResponse(items).toResponseEntity(SuccessCode.OK);
     }
 
     @GetMapping(path = "/v1/schedules/{id}")
-    public ResponseEntity findById(@PathVariable Long id){
+    public ResponseEntity<SuccessResponse> findById(@PathVariable Long id){
         ScheduleDto scheduleDto = scheduleService.findById(id);
         return new SuccessResponse(scheduleDto).toResponseEntity(SuccessCode.OK);
     }
 
     @PostMapping(path = "/v1/schedules")
-    public ResponseEntity create(@RequestBody @Validated ScheduleDto scheduleDto){
-        ScheduleDto result = scheduleService.insert(scheduleDto);
+    public ResponseEntity<SuccessResponse> create(@RequestBody @Validated ScheduleRequest scheduleRequest){
+        ScheduleDto result = scheduleService.insert(new ScheduleDto(scheduleRequest));
+        scheduleChangeDataSpreader.create(result);
         return new SuccessResponse(result).toResponseEntity(SuccessCode.CREATED);
     }
 
     @PutMapping(path = "/v1/schedules")
-    public ResponseEntity update(@RequestBody @Validated ScheduleDto scheduleDto){
-        ScheduleDto result = scheduleService.update(scheduleDto);
+    public ResponseEntity<SuccessResponse> update(@RequestBody @Validated ScheduleRequest scheduleRequest){
+        ScheduleDto result = scheduleService.update(new ScheduleDto(scheduleRequest));
+        scheduleChangeDataSpreader.update(result);
         return new SuccessResponse(result).toResponseEntity(SuccessCode.OK);
     }
 
     @DeleteMapping(path = "/v1/schedules/{id}")
-    public ResponseEntity delete(@PathVariable Long id){
+    public ResponseEntity<SuccessResponse> delete(@PathVariable Long id){
         scheduleService.delete(id);
+        scheduleChangeDataSpreader.delete(id);
         return new SuccessResponse(null).toResponseEntity(SuccessCode.OK);
     }
 
     @PutMapping(path = "/v1/schedules/run")
-    public ResponseEntity runUpdate(@RequestBody @Validated ScheduleRunDto scheduleRunDto){
-
+    public ResponseEntity<SuccessResponse> runUpdate(@RequestBody @Validated ScheduleRunRequest scheduleRunRequest){
+        ScheduleRunDto scheduleRunDto = new ScheduleRunDto(scheduleRunRequest);
         scheduleService.updateRun(scheduleRunDto);
         return new SuccessResponse(scheduleRunDto).toResponseEntity(SuccessCode.OK);
     }
