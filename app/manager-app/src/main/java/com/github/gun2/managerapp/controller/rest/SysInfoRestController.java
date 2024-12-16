7f9990445f995615e4e15d5486dc4ec582ebd2ad
gun2 <0dayz@naver.com>
2024-12-16 22:33:41 +0900

    feat: 시스템 정보 관련 api 추가 > websocket으로 제공하던 연결 세션, 성공, 실패 횟수 정보를 api로 추가

--- app/manager-app/src/main/java/com/github/gun2/managerapp/controller/rest/SysInfoRestController.java
+++ app/manager-app/src/main/java/com/github/gun2/managerapp/controller/rest/SysInfoRestController.java
@@ -3,10 +3,14 @@ package com.github.gun2.managerapp.controller.rest;
 import com.github.gun2.managerapp.component.AccessUserComponent;
 import com.github.gun2.managerapp.component.FailureCountComponent;
 import com.github.gun2.managerapp.component.SuccessCountComponent;
+import com.github.gun2.managerapp.constant.SuccessCode;
+import com.github.gun2.managerapp.form.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.WebSocketHandler;
@@ -16,7 +20,7 @@ import org.springframework.web.socket.messaging.SubProtocolWebSocketHandler;
 @RestController
 @RequiredArgsConstructor
 @MessageMapping("/sys-info")
-@RequestMapping("/sys-info")
+@RequestMapping("/api")
 public class SysInfoRestController {
     private final AccessUserComponent accessUserComponent;
     private final FailureCountComponent failureCountComponent;
@@ -24,6 +28,15 @@ public class SysInfoRestController {
     private final SimpMessageSendingOperations sendingOperations;
     private final WebSocketHandler webSocketHandler;
 
+    /**
+     * 현재 세션 연결 수 반환
+     * @return
+     */
+    @GetMapping("/v1/system-information/users")
+    public ResponseEntity<SuccessResponse<Integer>> getUserNumber(){
+        int sessionCnt = ((SubProtocolWebSocketHandler)webSocketHandler).getStats().getWebSocketSessions();
+        return new SuccessResponse(sessionCnt).toResponseEntity(SuccessCode.OK);
+    }
 
     @MessageMapping("/user/init")
     public void userNumber(String msg) {
@@ -31,11 +44,27 @@ public class SysInfoRestController {
         sendingOperations.convertAndSend("/sys-info/user", sessionCnt);
     }
 
+    /**
+     * 총 응답 성공 수 반환
+     * @return
+     */
+    @GetMapping("/v1/system-information/success-count")
+    public ResponseEntity<SuccessResponse<Integer>> getSuccessCount(){
+        return new SuccessResponse(successCountComponent.getNumber()).toResponseEntity(SuccessCode.OK);
+    }
     @MessageMapping("/success/init")
     public void successNumber(String msg) {
         sendingOperations.convertAndSend("/sys-info/success", successCountComponent.getNumber());
     }
 
+    /**
+     * 총 응답 실패 수 반환
+     * @return
+     */
+    @GetMapping("/v1/system-information/failure-count")
+    public ResponseEntity<SuccessResponse<Integer>> getFailureNumber(){
+        return new SuccessResponse(failureCountComponent.getNumber()).toResponseEntity(SuccessCode.OK);
+    }
     @MessageMapping("/failure/init")
     public void failureNumber(String msg) {
         sendingOperations.convertAndSend("/sys-info/failure", failureCountComponent.getNumber());
