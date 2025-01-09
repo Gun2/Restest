548a29e669afa25918139d9daba42e7759b838bc
gun2 <0dayz@naver.com>
2025-01-09 23:59:48 +0900

    fix: stomp client hook 성능개선

--- app/ui-app/src/hooks/useStompClient.ts
+++ app/ui-app/src/hooks/useStompClient.ts
@@ -1,16 +1,18 @@
 import {WebSocketClientFactory} from "utils/webSocketClientFactory";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Client} from "@stomp/stompjs";
 
 
 export const useStompClient = () => {
 
-    const instance = WebSocketClientFactory.getInstance();
-    const [client, setClient] = useState<Client | undefined>();
-    instance.getClient().then(value => {
-        setClient(value);
-    })
 
+    const [client, setClient] = useState<Client | undefined>();
+    useEffect(() => {
+        const instance = WebSocketClientFactory.getInstance();
+        instance.getClient().then(value => {
+            setClient(value);
+        })
+    }, []);
     return client;
 
 }
